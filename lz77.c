ed00262659351f6cdf020170182b3c27e086e180
Patrick Cookson <cookson.pat@gmail.com>
2015-03-09 19:23:48 -0400

    decode and encode working

--- lz77.c
+++ lz77.c
@@ -7,7 +7,100 @@
 
 void Decode_Using_LZ77(char *in_compressed_filename_Ptr, char *out_PGM_filename_Ptr)
 {
+    int height;
+    int width;
+    int arrLength;
+    FILE *decodeFile;
+    char line[80];
+    int endPtr;
+    int startPtr;
 
+    decodeFile = fopen(in_compressed_filename_Ptr, "rt");
+    //get height
+    fgets(line, 80, decodeFile);
+    sscanf(line, "%d", &height);
+
+    //get width
+    fgets(line, 80, decodeFile);
+    sscanf(line, "%d", &width);
+
+    //get array length
+    fgets(line, 80, decodeFile);
+    sscanf(line, "%d", &arrLength);
+
+    struct PGM_Image pgmImage;
+    create_PGM_Image(&pgmImage, width, height, 255);
+
+    int *offSetArr = malloc((pgmImage.width * pgmImage.height) * sizeof(int));
+    int *repArr = malloc((pgmImage.width * pgmImage.height) * sizeof(int));
+    int *misAllArr = malloc((pgmImage.width * pgmImage.height) * sizeof(int));
+    int *searchBuffer = malloc((pgmImage.width * pgmImage.height) * sizeof(int));
+
+    int i;
+    int temp;
+    //get offSetArr
+    for(i=0; i< arrLength;i++){
+        fgets(line, 80, decodeFile);
+        sscanf(line, "%d", &temp);
+        offSetArr[i] = temp;
+    }
+
+    //get repArr
+    for(i=0; i< arrLength;i++){
+        fgets(line, 80, decodeFile);
+        sscanf(line, "%d", &temp);
+        repArr[i] = temp;
+    }
+
+    //get misArr
+    for(i=0; i< arrLength;i++){
+        fgets(line, 80, decodeFile);
+        sscanf(line, "%d", &temp);
+        misAllArr[i] = temp;
+    }
+
+    int j;
+    int tempPtr;
+    height = 0;
+    width =0;
+    endPtr = 0;
+    startPtr = 0;
+    for(i=0;i<arrLength;i++){
+        //no repeating pattern. Value equal to misAllArr[i]
+        if(offSetArr[i] == 0 && repArr[i] == 0){
+            searchBuffer[endPtr] = misAllArr[i];
+            endPtr++;
+        }else{//there is a repeating pattern
+            tempPtr = endPtr;
+            for(j = tempPtr - offSetArr[i]; j<tempPtr - offSetArr[i] + repArr[i];j++){
+                searchBuffer[endPtr] = searchBuffer[j];
+                endPtr++;
+            }
+            searchBuffer[endPtr] = misAllArr[i];
+            endPtr++;
+        }
+
+    }
+
+
+    int index = 0;
+    for(height = 0; height < pgmImage.height; height++)
+    {
+        for(width = 0; width < pgmImage.width; width++)
+        {
+            pgmImage.image[height][width] = searchBuffer[index];
+
+            index++;
+        }
+    }
+
+    save_PGM_Image(&pgmImage, out_PGM_filename_Ptr, 1);
+    free_PGM_Image(&pgmImage);
+    //free arrays
+    free(searchBuffer);
+    free(offSetArr);
+    free(repArr);
+    free(misAllArr);
 }
 
 void Encode_Using_LZ77(char *in_PGM_filename_Ptr, unsigned int searching_buffer_size, char*compressed_filename_Ptr)
@@ -109,11 +202,38 @@ void Encode_Using_LZ77(char *in_PGM_filename_Ptr, unsigned int searching_buffer_
     }
 
 
-    int j;
-
-
 
     //place arrays and height and width into encoded file
+
+    FILE *encodedFile;
+    encodedFile = fopen(compressed_filename_Ptr, "w");
+    if(encodedFile == NULL)
+    {
+        printf("Error writing to file\n");
+        exit(1);
+    }
+
+    //print height and width of image to file
+    fprintf(encodedFile, "%d\n", pgmImage.height);
+    fprintf(encodedFile, "%d\n", pgmImage.width);
+    //print array length of three arrays
+    fprintf(encodedFile, "%d\n", indexArr);
+
+    //print arrays in order of offset, rep, mis
+    int j;
+    for(j=0; j<indexArr; j++)
+    {
+        fprintf(encodedFile, "%d\n", offSetArr[j]);
+    }
+    for(j=0; j<indexArr; j++)
+    {
+        fprintf(encodedFile, "%d\n", repArr[j]);
+    }
+    for(j=0; j<indexArr; j++)
+    {
+        fprintf(encodedFile, "%d\n", misAllArr[j]);
+    }
+    fclose(encodedFile);
 
     //free arrays
     free(searchBuffer);
@@ -136,25 +256,34 @@ int main(int argc, char *argv[])
     char *pgmFilename;
     char *compressedFileName;
 
-    if(argc!=5)
-    {
-        perror("Invalid command line inputs");
-        exit(1);
-    }
 
-    bufferSize = atoi(argv[1]);
-    function = strdup(argv[2]);
-    pgmFilename = strdup(argv[3]);
-    compressedFileName = strdup(argv[4]);
-
-    if(strcmp(function, "Encode") == 0)
+    function = strdup(argv[1]);
+    if(strcmp(function, "encode") == 0)
     {
+        if(argc!=5)
+        {
+            perror("Invalid command line inputs");
+            exit(1);
+        }
+
+        bufferSize = atoi(argv[2]);
+        pgmFilename = strdup(argv[3]);
+        compressedFileName = strdup(argv[4]);
         Encode_Using_LZ77(pgmFilename, bufferSize, compressedFileName);
     }
-    else
+    else if(strcmp(function, "decode") == 0)
     {
-        Decode_Using_LZ77(compressedFileName, strcat(compressedFileName, ".pgm"));
+        if(argc!=4)
+        {
+            perror("Invalid command line inputs");
+            exit(1);
+        }
+        compressedFileName = strdup(argv[2]);
+        pgmFilename = strdup(argv[3]);
+        Decode_Using_LZ77(compressedFileName, pgmFilename);
     }
+
+
 
 
 }
